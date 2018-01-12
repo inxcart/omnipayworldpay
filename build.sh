6a9c1058dabfed4575c07ae0e93380c5c98f843b
Michael Dekker <git@michaeldekker.nl>
2018-01-12 14:18:12 +0100

    Use local php scoper

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ rm build/ -rf
 # Composer install and scoping
 composer install --no-dev --prefer-dist
 mv vendor/ pre-scoper/
-php-scoper add-prefix -p ThirtyBeesMollie -n
+php ./php-scoper.phar add-prefix -p ThirtyBeesMollie -n
 
 # Cleanup
 mv build/pre-scoper/ vendor/
